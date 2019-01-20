08c641f8f4f908d4de1812039491233b4d7f22d0
yuliiadubinina <yuliiadubinina@gmail.com>
2019-01-20 13:46:47 +0200

    added slider

--- dist/index.js
+++ dist/index.js
@@ -1,68 +1,92 @@
-var btnStart = document.querySelector('.start');
-var imgFirst = document.querySelector('.imgFirst');
-var imgSecond = document.querySelector('.imgSecond');
-var imgThird = document.querySelector('.imgThird');
-var total = document.querySelector(".total");
-var lastWin = document.querySelector(".win");
+const btnStart = document.querySelector('.start');
+const imgFirst = document.querySelector('.imgFirst');
+const imgSecond = document.querySelector('.imgSecond');
+const imgThird = document.querySelector('.imgThird');
+const total = document.querySelector(".total");
+const lastWin = document.querySelector(".win");
 
-var startMp3 = document.querySelector(".play");
+const startMp3 = document.querySelector(".play");
 
-var items = [ 
-  { img: "img/plum.png", value: 10, id: 2},
-  { img: "img/bananas.png", value: 20, id: 1},
-  { img: "img/pear.png", value: 30, id: 2},
-  { img: "img/watermelon.png", value: 40, id: 2},
-  { img: "img/cherries.png", value: 50, id: 2},
-  { img: "img/strawberry.png", value: 60, id: 3},
-  { img: "img/clover.png", value: 70, id: 2},
-  { img: "img/seven.png", value: 100, id: 2},
-  { img: "img/bw.png", value: 200, id: 2},
-  { img: "img/jackpot.png", value: 300, id: 2},
+const items = [ 
+  { img: "img/plum.png", value: 10},
+  { img: "img/bananas.png", value: 20},
+  { img: "img/pear.png", value: 30},
+  { img: "img/watermelon.png", value: 40},
+  { img: "img/cherries.png", value: 50},
+  { img: "img/strawberry.png", value: 60},
+  { img: "img/clover.png", value: 70},
+  { img: "img/seven.png", value: 100},
+  { img: "img/bw.png", value: 200},
+  { img: "img/jackpot.png", value: 300}
 ];
 
 btnStart.onclick = () => {
   startMp3.play();
 
-  var firstRange = getRandomInRange(0, 9);
-  var secondRange = getRandomInRange(0, 9);
-  var thirdRange = getRandomInRange(0, 9);
-  
-  
-  setTimeout(setImg, 3200);
-  function setImg() {
-    imgFirst.setAttribute("src", items[firstRange].img);
-  };
+  setImage(imgFirst, 0);
+  setImage(imgFirst, 1000);
+  setImage(imgFirst, 2000);
 
-  setTimeout(setImg1, 3500);
-  function setImg1() {
+  setImage(imgSecond, 0);
+  setImage(imgSecond, 1000);
+  setImage(imgSecond, 2000);
+
+  setImage(imgThird, 0);
+  setImage(imgThird, 1000);
+  setImage(imgThird, 2000);
+  
+  const firstRange = getRandomInRange(0, 9);
+  const secondRange = getRandomInRange(0, 9);
+  const thirdRange = getRandomInRange(0, 9);
+  
+  setTimeout(() => {
+    imgFirst.setAttribute("src", items[firstRange].img)
+  }, 3200);
+
+  setTimeout(() => {
     imgSecond.setAttribute("src", items[secondRange].img)
-  };
+  }, 3500)
 
-  setTimeout(setImg2, 4000);
-  function setImg2() {
-    imgThird.setAttribute("src", items[thirdRange].img);
-  };
+  setTimeout(() => {
+    imgThird.setAttribute("src", items[thirdRange].img)
+  }, 4000)
 
   if (items[firstRange].value == items[secondRange].value && items[secondRange].value == items[thirdRange].value) {
-    var win = items[firstRange].value * 3;
-    var newTotal = +total.innerHTML + win;
+    const win = items[firstRange].value * 3;
+    const newTotal = +total.innerHTML + win;
     total.innerHTML = newTotal;
     lastWin.innerHTML = win;
   } else if (items[firstRange].value === items[secondRange].value || items[firstRange].value === items[thirdRange].value) {
-    var win = items[firstRange].value;
+    const win = items[firstRange].value;
     total.innerHTML = +total.innerHTML + win;
     lastWin.innerHTML = win;
   } else if (items[secondRange].value == items[thirdRange].value) {
-    var win = items[secondRange].value;
+    const win = items[secondRange].value;
     total.innerHTML = +total.innerHTML + win;
 
     lastWin.innerHTML = win;
   } else {
     total.innerHTML = +total.innerHTML - 50;
   }
-  
 }
 
 function getRandomInRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function setImg(img, v) {
+  for (let i = 0; i < v; i++) {
+    const url = items[i].img;
+    img.setAttribute("src", url);
+  }
+}
+
+function setImage(img, int) {
+  for (let i = 1; i <= 10; i++) {
+    let time = +(i + "00") + int;
+    setTimeout(() => {
+      setImg(img, i);
+    }, time);
+    console.log(time);
+  }
 }
